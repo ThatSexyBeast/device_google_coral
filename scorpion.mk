567bb161661260014c4f5f83639220e9d1db6589
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    floral: Initial Scorpion bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ scorpion.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2021 The ScorpionROM Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit device configuration, quick-fix so we don't have to rework our build repo
+$(call inherit-product, device/google/coral/scorpion_coral.mk)
