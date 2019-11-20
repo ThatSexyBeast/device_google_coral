4f4cd206a656bb7f884a0d61766739ce3608c067
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    floral: Let's get Crooked
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ crooked_coral.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The Crooked Android Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/crooked/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/aosp_coral.mk)
+
+include device/google/coral/device-crooked.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4 XL
+PRODUCT_NAME := crooked_coral
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=coral \
+    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys
